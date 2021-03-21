78f5fbc23b203e23c11d10227a89d0d3e6aa8602
sahurabi92 <sahurabi92@gmail.com>
2021-03-22 04:11:27 +0530

    Update start.py

--- service/start.py
+++ service/start.py
@@ -3,4 +3,4 @@ from apps import create_app
 app, db = create_app()
 app.app_context().push()
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=8080)
